c94279ae39d899b5129b64e7bc60d90214c719c4
evandroloschi <evandro.loschi@gmail.com>
2014-12-15 11:48:52 -0200

    Detector de faces melhorado
    O detector de faces agora possui duas opções: delinear os rostos com um quadro vermelho ou enrubescê-los.

--- Detecção de Face/FaceDetect.py
+++ Detecção de Face/FaceDetect.py
@@ -1,8 +1,35 @@
 # -*- coding: utf-8 -*-
 import cv2
+import Image
+import numpy
 
 # Caminho padrão para o o haar cascade
 CASC_PATH = 'haarcascade_frontalface_default.xml'
+
+def desenha(entrada,saida, coordenadas, mode=None):
+	try:
+		img = numpy.array(Image.open(entrada))
+
+		for c in coordenadas:
+			if mode=='fill':
+				# Red layer.
+				img[c[1]:c[1]+c[2], c[0]:c[0]+c[2], 0] = 255
+			else:
+				# Red layer.
+				img[c[1]:c[1]+5, c[0]:c[0]+c[2], 0] = 255
+				img[c[1]+c[2]:c[1]+c[2]+5, c[0]:c[0]+c[2], 0] = 255
+				img[c[1]:c[1]+c[2], c[0]:c[0]+5, 0] = 255
+				img[c[1]:c[1]+c[2], c[0]+c[2]:c[0]+c[2]+5, 0] = 255
+
+				# Green and blue layer.
+				img[c[1]:c[1]+5, c[0]:c[0]+c[2], 1:] = 0
+				img[c[1]+c[2]:c[1]+c[2]+5, c[0]:c[0]+c[2], 1:] = 0
+				img[c[1]:c[1]+c[2], c[0]:c[0]+5, 1:] = 0
+				img[c[1]:c[1]+c[2], c[0]+c[2]:c[0]+c[2]+5, 1:] = 0
+
+		Image.fromarray(img).save(saida)
+	except:
+		raise
 
 def reconhecimento(imagePath, cascPath = CASC_PATH, sf = 1.1, mn = 5, ms = (30, 30), f = cv2.cv.CV_HAAR_SCALE_IMAGE):
     # Criando o haar cascade
@@ -23,3 +50,8 @@ def reconhecimento(imagePath, cascPath = CASC_PATH, sf = 1.1, mn = 5, ms = (30, 
 
     # Retorna uma mensagem com as coordenadas das faces encontradas
     return faces
+
+if __name__=='__main__':
+	l = reconhecimento('pessoas.jpeg','haarcascade_frontalface_default.xml')
+	desenha('pessoas.jpeg', 'caras1.jpg', l)
+	desenha('pessoas.jpeg', 'caras2.jpg', l, 'fill')
